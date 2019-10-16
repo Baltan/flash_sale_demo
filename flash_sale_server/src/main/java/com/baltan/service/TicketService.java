0b949bae496627d98fe38849133ccd951d8e9980
Baltan <617640006@qq.com>
2019-10-16 11:33:23 +0800

    火车票Service类

---
+++ flash_sale_server/src/main/java/com/baltan/service/TicketService.java
@@ -0,0 +1,35 @@
+package com.baltan.service;
+
+import com.baltan.entity.Ticket;
+import com.baltan.mapper.TicketMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Description:
+ *
+ * @author Baltan
+ * @date 2019-10-15 16:46
+ */
+@Component
+public class TicketService {
+    @Autowired
+    private TicketMapper ticketMapper;
+
+    public String buyTicket(Map<String, Object> map) {
+        int ticketCount = ticketMapper.buyTicket(map);
+
+        if (ticketCount == 1) {
+            return "购票成功！";
+        } else {
+            return "购票失败！";
+        }
+    }
+
+    public List<Ticket> queryAvailableAllTickets(String from, String to) {
+        return ticketMapper.queryAvailableAllTickets(from, to);
+    }
+}
