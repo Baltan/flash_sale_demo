831909d8919fa4925d53aaa88f7cfb7bfe251327
Baltan <617640006@qq.com>
2019-10-16 23:09:42 +0800

    添加通过消息队列秒杀抢票的测试方法

--- flash_sale_client/src/test/java/com/baltan/FlashSaleClientApplicationTests.java
+++ flash_sale_client/src/test/java/com/baltan/FlashSaleClientApplicationTests.java
@@ -1,6 +1,8 @@
 package com.baltan;
 
+import com.baltan.config.RabbitMqConfig;
 import com.baltan.entity.Ticket;
+import com.baltan.util.RabbitMqUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,10 @@ import java.util.concurrent.CyclicBarrier;
 public class FlashSaleClientApplicationTests {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private RabbitMqUtil rabbitMqUtil;
+    @Autowired
+    private RabbitMqConfig rabbitMqConfig;
     private int concurrentCount = 1000;
     private CyclicBarrier cyclicBarrier = new CyclicBarrier(concurrentCount);
     private String url = "http://127.0.0.1:8080/";
@@ -71,6 +77,53 @@ public class FlashSaleClientApplicationTests {
                 System.out.println("用户" + j + "开始抢票");
                 String response = restTemplate.postForObject(url + "buyTicket", saleMap, String.class);
                 System.out.println("用户" + j + " ： " + response);
+            }, "用户" + j + "Thread").start();
+        }
+    }
+
+    @Test
+    public void testBuyTicketWithMQ() {
+        for (int i = 1; i <= concurrentCount; i++) {
+            ParameterizedTypeReference<List<Ticket>> typeReference =
+                    new ParameterizedTypeReference<List<Ticket>>() {
+                    };
+            /**
+             * 查询所有余票
+             */
+            ResponseEntity<List<Ticket>> responseEntity =
+                    restTemplate.exchange(url + "queryAllAvailableTickets" + "?from=" + from + "&to=" + to,
+                            HttpMethod.GET, new HttpEntity<>(Ticket.class), typeReference);
+            List<Ticket> availableTicketList = responseEntity.getBody();
+            int ticketCount = availableTicketList.size();
+            System.out.println("用户" + i + "查询结果，还剩余票： " + ticketCount + "张！");
+            int ticketNo = random.nextInt(ticketCount);
+            Ticket ticket = availableTicketList.get(ticketNo);
+
+            final int j = i;
+
+            Map<String, Object> saleMap = new HashMap<>();
+
+            saleMap.put("from", from);
+            saleMap.put("to", to);
+            saleMap.put("coachNo", ticket.getCoachNo());
+            saleMap.put("seatNo", ticket.getSeatNo());
+            saleMap.put("username", "用户" + j);
+            saleMap.put("userId", String.format("%018d", j));
+
+            String exchanger = rabbitMqConfig.getTicketRequestExchangerName();
+            String routeKey = rabbitMqConfig.getTicketRequestRouteKey();
+
+            new Thread(() -> {
+                try {
+                    cyclicBarrier.await();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                /**
+                 * 随机购买一张车票
+                 */
+                System.out.println("用户" + j + "开始抢票");
+                rabbitMqUtil.send(exchanger, routeKey, saleMap);
             }, "用户" + j + "Thread").start();
         }
     }
