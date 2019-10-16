9a4cdf3e02e332c63c7a461cddccd0e923326445
Baltan <617640006@qq.com>
2019-10-16 11:34:54 +0800

    票务系统服务端启动类

---
+++ flash_sale_server/src/main/java/com/baltan/FlashSaleServerApplication.java
@@ -0,0 +1,12 @@
+package com.baltan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashSaleServerApplication.class, args);
+    }
+}
