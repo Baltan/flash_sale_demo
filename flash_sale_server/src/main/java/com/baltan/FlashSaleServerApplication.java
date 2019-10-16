2c2ca1340f5be2ec5c5fd32fc9defdecfdcd87ac
Baltan <617640006@qq.com>
2019-10-16 23:07:55 +0800

    添加如何启动RabbieMQ的注释

--- flash_sale_server/src/main/java/com/baltan/FlashSaleServerApplication.java
+++ flash_sale_server/src/main/java/com/baltan/FlashSaleServerApplication.java
@@ -3,9 +3,21 @@ package com.baltan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <pre>
+ * 启动RabbitMQ流程：
+ * 1、进入/usr/local/Cellar/rabbitmq/版本号/sbin目录
+ * 2、执行rabbitmq-server -detached命令后台启动RabbitMQ
+ * 3、可以通过rabbitmqctl status命令查看RabbitMQ状态
+ * 4、可以在浏览器中访问http://localhost:15672查看RabbitMQ可视化界面
+ *
+ * 关闭RabbitMQ流程：
+ * 1、进入/usr/local/Cellar/rabbitmq/版本号/sbin目录
+ * 2、执行rabbitmqctl stop命令
+ * </pre>
+ */
 @SpringBootApplication
 public class FlashSaleServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FlashSaleServerApplication.class, args);
     }
